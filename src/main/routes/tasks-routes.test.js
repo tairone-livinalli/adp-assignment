644d2ab81dae15d3e1b2987bf1ac5327ed62deb3
Tairone Livinalli <tairone.livinalli@softplan.com.br>
2021-02-08 07:57:54 -0300

    Added MathRoute integration test to ensure 200 response when a valid task is provided

---
+++ src/main/routes/tasks-routes.test.js
@@ -0,0 +1,22 @@
+const request = require('supertest')
+const app = require('../config/app')
+
+describe('Math Routes', () => {
+  test('Should return 200 when valid task provided', async () => {
+    const id = '66df7d3d-8340-4efd-a528-b5204d02a864'
+
+    await request(app)
+      .post('/api/tasks')
+      .send({
+        id,
+        operation: 'multiplication',
+        left: -3364257091338055,
+        right: -6634491299249283
+      })
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.id).toEqual(id)
+        expect(body.result).toEqual(2.232013440092003e+31)
+      })
+  })
+})
