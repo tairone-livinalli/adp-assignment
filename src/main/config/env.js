9ddcadbf6844443c76580fe1dfd74a24ccbcadb8
Tairone Livinalli <tairone.livinalli@softplan.com.br>
2021-02-08 08:24:56 -0300

    Added circular api calls getting and validating tasks

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,3 +1,4 @@
 module.exports = {
-  port: process.env.PORT || 3333
+  port: process.env.PORT || 3333,
+  adpBaseUrl: process.env.ADP_BASE_URL || 'https://interview.adpeai.com/api/v1'
 }
--- src/main/index.js
+++ src/main/index.js
@@ -1,4 +1,29 @@
 const app = require('./config/app')
 const env = require('./config/env')
+const axios = require('axios')
 
-app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
+app.listen(env.port, async () => {
+  const localApiBaseUrl = `http://localhost:${env.port}`
+  console.log(`Server running at ${localApiBaseUrl}`)
+
+  try {
+    setInterval(async () => {
+      const { data: task } = await axios.get(`${env.adpBaseUrl}/get-task`)
+
+      console.log('-------------------------------')
+      console.log('Received task:')
+      console.log(`Id: ${task.id}`)
+      console.log(`Operation: ${task.operation}`)
+      console.log(`Left: ${task.left}`)
+      console.log(`Right: ${task.right}`)
+
+      const { data: calculatedTask } = await axios.post(`${localApiBaseUrl}/api/tasks`, { ...task })
+
+      const { data: isTaskCorrect } = await axios.post(`${env.adpBaseUrl}/submit-task`, { ...calculatedTask })
+
+      console.log(`Processed task of id: ${task.id} and result: ${calculatedTask.result} is: ${isTaskCorrect}`)
+    }, 5000)
+  } catch (e) {
+    console.error(e)
+  }
+})
