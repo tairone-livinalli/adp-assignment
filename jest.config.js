60990b155482e114efb1a0306a7e7b6dc72380ff
Tairone Livinalli <tairone.livinalli@softplan.com.br>
2021-02-08 07:03:50 -0300

    Added ignore to the main module

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   testEnvironment: 'node',
-  collectCoverageFrom: ['**/src/**/*.js']
+  collectCoverageFrom: ['**/src/**/*.js', '!**/src/main/**']
 }
