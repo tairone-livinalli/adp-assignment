ab0b437351c3e5832d16d5ea1ccb6cec25a7b368
Tairone Livinalli <tairone.livinalli@softplan.com.br>
2021-02-08 01:51:57 -0300

    Ensure MathUseCase throws missing param error if no left operator is provided

--- src/domain/usecases/math-usecase.spec.js
+++ src/domain/usecases/math-usecase.spec.js
@@ -1,12 +1,15 @@
 const { MissingParamError } = require('../../utils/errors')
 
 class MathUseCase {
-  calculate (id, operation) {
+  calculate (id, operation, left) {
     if (!id) {
       throw new MissingParamError('id')
     }
     if (!operation) {
       throw new MissingParamError('operation')
+    }
+    if (!left) {
+      throw new MissingParamError('left')
     }
   }
 }
@@ -20,5 +23,10 @@ describe('Math UseCase', () => {
   test('Should throw if no operation is provided', () => {
     const sut = new MathUseCase()
     expect(() => sut.calculate('id')).toThrow(new MissingParamError('operation'))
+  })
+
+  test('Should throw if no left operator is provided', () => {
+    const sut = new MathUseCase()
+    expect(() => sut.calculate('id', 'operation')).toThrow(new MissingParamError('left'))
   })
 })
