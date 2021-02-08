b53b15bdb43673858e08ff92305309f22404e678
Tairone Livinalli <tairone.livinalli@softplan.com.br>
2021-02-08 07:30:57 -0300

    Move MathRouter composition to its own file

---
+++ src/main/composers/tasks-router-composer.js
@@ -0,0 +1,17 @@
+const MathRouter = require('../../presentation/routers/math-router')
+const MathUseCase = require('../../domain/usecases/math-usecase')
+const AdditionUseCase = require('../../domain/usecases/addition-usecase')
+const SubtractionUseCase = require('../../domain/usecases/subtraction-usecase')
+const MultiplicationUseCase = require('../../domain/usecases/multiplication-usecase')
+const DivisionUseCase = require('../../domain/usecases/division-usecase')
+const RemainderUseCase = require('../../domain/usecases/remainder-usecase')
+
+const additionUseCase = new AdditionUseCase()
+const subtractionUseCase = new SubtractionUseCase()
+const multiplicationUseCase = new MultiplicationUseCase()
+const divisionUseCase = new DivisionUseCase()
+const remainderUseCase = new RemainderUseCase()
+const mathUseCase = new MathUseCase({ additionUseCase, subtractionUseCase, multiplicationUseCase, divisionUseCase, remainderUseCase })
+const mathRouter = new MathRouter(mathUseCase)
+
+module.exports = mathRouter
--- src/main/routes/tasks-routes.js
+++ src/main/routes/tasks-routes.js
@@ -1,3 +1,5 @@
+const mathRouter = require('../composers/tasks-router-composer')
+
 module.exports = router => {
-  router.post('/tasks')
+  router.post('/tasks', mathRouter.route)
 }
