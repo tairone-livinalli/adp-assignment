9201ff5d2596a89f7b00a84c832f739d9e669740
Tairone Livinalli <tairone.livinalli@softplan.com.br>
2021-02-08 07:59:23 -0300

    Added MathRoute integration test to ensure 400 response when an invalid task is provided

--- src/main/routes/tasks-routes.test.js
+++ src/main/routes/tasks-routes.test.js
@@ -2,7 +2,7 @@ const request = require('supertest')
 const app = require('../config/app')
 
 describe('Math Routes', () => {
-  test('Should return 200 when valid task provided', async () => {
+  test('Should return 200 when valid task is provided', async () => {
     const id = '66df7d3d-8340-4efd-a528-b5204d02a864'
 
     await request(app)
@@ -18,5 +18,18 @@ describe('Math Routes', () => {
         expect(body.id).toEqual(id)
         expect(body.result).toEqual(2.232013440092003e+31)
       })
+  })
+
+  test('Should return 400 when invalid task is provided', async () => {
+    const id = '66df7d3d-8340-4efd-a528-b5204d02a864'
+
+    await request(app)
+      .post('/api/tasks')
+      .send({
+        id,
+        left: -3364257091338055,
+        right: -6634491299249283
+      })
+      .expect(400)
   })
 })
