9f57a83c32bf65e80e3f40c2c7bd5834703257c5
Tairone Livinalli <tairone.livinalli@softplan.com.br>
2021-02-08 05:30:23 -0300

    Ensure SubtractionUseCase sub with two positive numbers

--- src/domain/usecases/subtraction-usecase.spec.js
+++ src/domain/usecases/subtraction-usecase.spec.js
@@ -9,6 +9,8 @@ const makeSut = () => {
       if (!right) {
         throw new MissingParamError('right')
       }
+
+      return left - right
     }
   }
 
@@ -24,5 +26,11 @@ describe('Subtraction UseCase', () => {
   test('Should throw if no right operator is provided', () => {
     const sut = makeSut()
     expect(() => sut.sub('left')).toThrow(new MissingParamError('right'))
+  })
+
+  test('Should sub with two positive numbers', () => {
+    const sut = makeSut()
+    const result = sut.sub(5, 3)
+    expect(result).toEqual(2)
   })
 })
