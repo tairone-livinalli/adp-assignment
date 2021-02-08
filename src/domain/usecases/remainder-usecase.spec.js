f6de71c4582c72708b5457e2a14f4a651148e380
Tairone Livinalli <tairone.livinalli@softplan.com.br>
2021-02-08 06:03:09 -0300

    Ensure RemainderUseCase get the rest with negative left operator

--- src/domain/usecases/remainder-usecase.spec.js
+++ src/domain/usecases/remainder-usecase.spec.js
@@ -33,4 +33,10 @@ describe('Remainder UseCase', () => {
     const result = sut.rest(7420122351257453, 8773915326797283)
     expect(result).toEqual(7420122351257453)
   })
+
+  test('Should get the rest with negative left operator', () => {
+    const sut = makeSut()
+    const result = sut.rest(-8773915326797283, 7420122351257453)
+    expect(result).toEqual(-1353792975539830)
+  })
 })
